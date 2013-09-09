766c7070c2711221057307d3128612c6e7f940f1
Yifeng Jiang <uprushworld@gmail.com>
2013-09-09 17:25:22 +0900

    fix bootaws.sh

--- scripts/bootaws.sh
+++ scripts/bootaws.sh
@@ -4,21 +4,26 @@
 #
 
 # install pip
-sudo apt-get install python-pip
+sudo apt-get install -y python-pip
 
 # install AWS CLI
 sudo pip install awscli
 
-aws ec2 describe-instances
-
-mkdir ~/.aws
+mkdir -p ~/.aws
 chmod 700 ~/.aws
 
-cat <<-EOH > ~/aws/config
+cat <<-EOH > ~/.aws/config
 [default]
 aws_access_key_id = <YOUR_KEY>
 aws_secret_access_key = <YOUR_SECRET_KEY>
-region = us_west_2
+region = <DEFAULT_REGION>
 EOH
 
 chmod 700 ~/.aws/config
+
+echo 
+echo "DONE"
+echo "Do NOT forget to change your AWS key in ~/.aws/config"
+echo "After that run 'aws ec2 describe-instances' to confirm AWS CLI setup."
+echo
+
