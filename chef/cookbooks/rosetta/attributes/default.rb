3acbc86050e75df9f1e1a353febd6b6c5a038d12
Yifeng Jiang <uprushworld@gmail.com>
2013-10-25 15:08:44 +0900

    output to s3

--- chef/cookbooks/rosetta/attributes/default.rb
+++ chef/cookbooks/rosetta/attributes/default.rb
@@ -10,3 +10,12 @@ default['rosetta']['broker']['redis_db'] = 0
 
 ## filter ##
 default['rosetta']['filter']['flush_size'] = 100
+default['rosetta']['filter']['s3']['access_key_id'] = ""
+default['rosetta']['filter']['s3']['secret_access_key'] = ""
+default['rosetta']['filter']['s3']['bucket'] = ""
+default['rosetta']['filter']['s3']['endpoint_region'] = "us-east-1"
+default['rosetta']['filter']['s3']['size_file'] = 0
+default['rosetta']['filter']['s3']['time_file'] = 0
+default['rosetta']['filter']['s3']['codec'] = 'json'
+default['rosetta']['filter']['s3']['format'] = 'json'
+default['rosetta']['filter']['s3']['use_ssl'] = false
