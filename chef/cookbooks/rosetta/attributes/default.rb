3d3dcb832691a5bfef0eea60987545c8abef74f8
Yifeng Jiang <uprushworld@gmail.com>
2013-10-31 16:54:26 +0900

    add apache-loggen init script

--- chef/cookbooks/rosetta/attributes/default.rb
+++ chef/cookbooks/rosetta/attributes/default.rb
@@ -1,8 +1,9 @@
 ## agent ##
 default['rosetta']['agent']['include_loggen'] = false
-default['rosetta']['agent']['apache_loggen']['bin'] = '/home/vagrant/.rbenv/shims/apache-loggen'
-default['rosetta']['agent']['apache_loggen']['options'] = '--rate 2 --rotate 86400'
-default['rosetta']['agent']['apache_access_log'] = '/var/log/apache2/access.log'
+default['rosetta']['agent']['loggen_bin'] = '/home/vagrant/.rbenv/shims/apache-loggen'
+default['rosetta']['agent']['loggen_options'] = '--rate 2 --rotate 86400'
+default['rosetta']['agent']['apache_access_dir'] = '/var/log/apache2'
+default['rosetta']['agent']['apache_access_file'] = 'access.log'
 
 
 ## broker ##
--- chef/cookbooks/rosetta/recipes/agent.rb
+++ chef/cookbooks/rosetta/recipes/agent.rb
@@ -5,6 +5,13 @@ if node['rosetta']['agent']['include_loggen']
   package "ruby-dev" # install mkmf, which is required by apache-loggen
   gem_package "apache-loggen"
 
+  # create log dir
+  directory node['rosetta']['agent']['apache_access_dir'] do
+    owner "root"
+    group "root"
+    action :create
+  end
+
   # create apache-loggen init script
   template "/etc/init.d/apache-loggen" do
     source "apache-loggen/init.erb"
