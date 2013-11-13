a634fed56613271a19b8aa77ebef24465e033181
Yifeng Jiang <uprushworld@gmail.com>
2013-11-13 10:05:01 +0900

    more sample commands

--- aws/commands.sh
+++ aws/commands.sh
@@ -38,7 +38,16 @@ aws elasticache create-cache-cluster
 # {"host":"104.129.146.40","user":null,"method":"GET","path":"/item/finance/806","code":200,"size":119,"referer":"/search/?c=Cameras+Health","agent":"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)","@node":"ip-172-31-11-77","@timestamp":"2013-11-06T09:08:51.000Z","@version":"1","type":"apache_access","tags":["apache_access"],"geoip":{"country_code2":"US"}}
 
 
+#### Kibana
+# QUERY: games
+# (games OR books) AND size:[130 TO 200]
+# My Dashboard
+
+
 #### HIVE ####
+# login
+ssh hadoop@ec2-54-201-53-129.us-west-2.compute.amazonaws.com
+
 # create hive table
 CREATE  EXTERNAL  TABLE apache_logs
 (
@@ -48,7 +57,7 @@ ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' STORED AS TEXTFILE
 LOCATION  's3://rosetta-logs/apache';
 
 # sample query
-select count(1) from apache_logs;
+select a.* from apache_logs a limit 1;
 
 # sample query
 select b.*
@@ -58,6 +67,7 @@ as timestamp, code, path
 where b.code != 200
 limit 100;
 
+
 # convert to csv, save in S3 (to load into RedShift)
 INSERT OVERWRITE DIRECTORY 's3://rosetta-logs/csv'
 select concat(b.timestamp, ',', b.code, ',', b.path)
@@ -66,26 +76,28 @@ LATERAL VIEW json_tuple(a.log, '@timestamp', 'code', 'path') b
 as timestamp, code, path
 where b.code > 0;
 
+# convert to csv (sample record)
+head ~/services/bigdata/demo/sample.csv
 
 #### REDSHIFT ####
 psql -d mydb -h mydb.cqsxlytconbn.us-west-2.redshift.amazonaws.com -p 5439 -U awsuser -W
 
-create table apache_logs (timestamp char(24), code int, path varchar)
+create table apache_logs (timestamp char(24), code int, path varchar);
 
 # copy apache_logs from 's3://rosetta-logs/csv'
 # credentials 'aws_access_key_id=<access-key-id>;aws_secret_access_key=<secret-access-key>';
 
 copy apache_logs from 's3://rosetta-logs/csv/000'
-credentials 'aws_access_key_id=AKIAIN6E3NPHQWNSVD3A;aws_secret_access_key=ukpus3BnbwWCQtUCe4l5ElGCmF0D1IYn7fNVcrrA'
+credentials 'aws_access_key_id=<YOUR_KEY>;aws_secret_access_key=<SECRET_KEY>'
 delimiter ',';
 
 select * from apache_logs where code != 200;
 
 
 # s3distcp
-./elastic-mapreduce --jobflow j-3GY8JC4179IOJ --jar \
-/home/hadoop/lib/emr-s3distcp-1.0.jar \
---arg --s3Endpoint --arg 's3-us-west-2.amazonaws.com' \
---arg --src --arg 's3://rosetta-logs/apache/' \
---arg --dest --arg 's3://rosetta-logs/archive/' \
---arg --srcPattern --arg '*.txt'
+# ./elastic-mapreduce --jobflow j-3GY8JC4179IOJ --jar \
+# /home/hadoop/lib/emr-s3distcp-1.0.jar \
+# --arg --s3Endpoint --arg 's3-us-west-2.amazonaws.com' \
+# --arg --src --arg 's3://rosetta-logs/apache/' \
+# --arg --dest --arg 's3://rosetta-logs/archive/' \
+# --arg --srcPattern --arg '*.txt'
